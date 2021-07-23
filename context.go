ad4faacd64289e808823b8ba436308676739ac4b
Eze <grosfeldezekiel@gmail.com>
2021-07-23 18:26:13 -0300

    Adding param and query

--- context.go
+++ context.go
@@ -9,3 +9,13 @@ func (c *Context) Next() error {
 func (c *Context) GetHeader(key string) []byte {
 	return c.Parent.Request.Header.Peek(key)
 }
+
+// Param returns the value of a given param
+func (c *Context) Param(key string) interface{} {
+	return c.Parent.UserValue(key)
+}
+
+// Query returns the value of a given query key
+func (c *Context) Query(key string) []byte {
+	return c.Parent.QueryArgs().Peek(key)
+}
