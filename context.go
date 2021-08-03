28e3d9adf538d69812beb346be393aced1e11301
Eze <grosfeldezekiel@gmail.com>
2021-08-03 20:38:02 -0300

    Adding context functions

--- context.go
+++ context.go
@@ -1,5 +1,7 @@
 package yoda
 
+import "context"
+
 // Next function
 func (c *Context) Next() error {
 	return c.Parent.Next()
@@ -18,4 +20,24 @@ func (c *Context) Param(key string) interface{} {
 // Query returns the value of a given query key
 func (c *Context) Query(key string) []byte {
 	return c.Parent.QueryArgs().Peek(key)
+}
+
+// SetValue sets a value with a given key
+func (c *Context) SetValue(key string, value interface{}) {
+	c.Parent.SetUserValue(key, value)
+}
+
+// GetValue gets the value set by SetValue
+func (c *Context) GetValue(key string) interface{} {
+	return c.Parent.UserValue(key)
+}
+
+// AttachContext adds a new context to the current one
+func (c *Context) AttachContext(context context.Context) {
+	c.Parent.AttachContext(context)
+}
+
+// AttachedContext returns the attached context
+func (c *Context) AttachedContext() context.Context {
+	return c.Parent.AttachedContext()
 }
