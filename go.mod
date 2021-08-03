c80b425af15fc1fd55b26965c15412cd800d4f3c
Eze <grosfeldezekiel@gmail.com>
2021-08-02 21:17:42 -0300

    Fixes and updates

--- go.mod
+++ go.mod
@@ -3,11 +3,15 @@ module github.com/ezegrosfeld/yoda
 go 1.16
 
 require (
+	github.com/andybalholm/brotli v1.0.3 // indirect
 	github.com/atreugo/cors v1.0.5
 	github.com/ezegrosfeld/vader v0.1.2
+	github.com/fasthttp/router v1.4.1 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/klauspost/compress v1.13.2 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/savsgio/atreugo/v11 v11.8.0
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
--- router.go
+++ router.go
@@ -1,100 +1,91 @@
 package yoda
 
-import "github.com/savsgio/atreugo/v11"
+import (
+	"github.com/savsgio/atreugo/v11"
+)
 
 // Get creates a listener of GET requests
 func (r *Router) Get(path string, handler Handler, middlewares ...Handler) {
+	fns := []atreugo.Middleware{}
+
+	for _, fn := range middlewares {
+		fns = append(fns, func(rc *atreugo.RequestCtx) error {
+			ctx := &Context{rc}
+			return fn(ctx)
+		})
+	}
+
 	r.Parent.GET(path, func(rc *atreugo.RequestCtx) error {
 		ctx := &Context{rc}
 		return handler(ctx)
-	})
-
-	for _, fn := range middlewares {
-		r.Parent.UseBefore(func(rc *atreugo.RequestCtx) error {
-			if string(rc.Path()) == path {
-				ctx := &Context{rc}
-				return fn(ctx)
-			} else {
-				return rc.Next()
-			}
-		})
-	}
+	}).UseBefore(fns...)
 }
 
 // Post creates a listener of POST requests
 func (r *Router) Post(path string, handler Handler, middlewares ...Handler) {
+	fns := []atreugo.Middleware{}
+
+	for _, fn := range middlewares {
+		fns = append(fns, func(rc *atreugo.RequestCtx) error {
+			ctx := &Context{rc}
+			return fn(ctx)
+		})
+	}
+
 	r.Parent.POST(path, func(rc *atreugo.RequestCtx) error {
 		ctx := &Context{rc}
 		return handler(ctx)
-	})
+	}).UseBefore(fns...)
 
-	for _, fn := range middlewares {
-		r.Parent.UseBefore(func(rc *atreugo.RequestCtx) error {
-			if string(rc.Path()) == path {
-				ctx := &Context{rc}
-				return fn(ctx)
-			} else {
-				return rc.Next()
-			}
-		})
-	}
 }
 
 // Put creates a listener of PUT requests
 func (r *Router) Put(path string, handler Handler, middlewares ...Handler) {
+	fns := []atreugo.Middleware{}
+
+	for _, fn := range middlewares {
+		fns = append(fns, func(rc *atreugo.RequestCtx) error {
+			ctx := &Context{rc}
+			return fn(ctx)
+		})
+	}
+
 	r.Parent.PUT(path, func(rc *atreugo.RequestCtx) error {
 		ctx := &Context{rc}
 		return handler(ctx)
-	})
-
-	for _, fn := range middlewares {
-		r.Parent.UseBefore(func(rc *atreugo.RequestCtx) error {
-			if string(rc.Path()) == path {
-				ctx := &Context{rc}
-				return fn(ctx)
-			} else {
-				return rc.Next()
-			}
-		})
-	}
+	}).UseBefore(fns...)
 }
 
 // PATCH creates a listener of PATCH requests
 func (r *Router) Patch(path string, handler Handler, middlewares ...Handler) {
+	fns := []atreugo.Middleware{}
+
+	for _, fn := range middlewares {
+		fns = append(fns, func(rc *atreugo.RequestCtx) error {
+			ctx := &Context{rc}
+			return fn(ctx)
+		})
+	}
 	r.Parent.PATCH(path, func(rc *atreugo.RequestCtx) error {
 		ctx := &Context{rc}
 		return handler(ctx)
-	})
-
-	for _, fn := range middlewares {
-		r.Parent.UseBefore(func(rc *atreugo.RequestCtx) error {
-			if string(rc.Path()) == path {
-				ctx := &Context{rc}
-				return fn(ctx)
-			} else {
-				return rc.Next()
-			}
-		})
-	}
+	}).UseBefore(fns...)
 }
 
 // Delete creates a listener of DELETE requests
 func (r *Router) Delete(path string, handler Handler, middlewares ...Handler) {
+	fns := []atreugo.Middleware{}
+
+	for _, fn := range middlewares {
+		fns = append(fns, func(rc *atreugo.RequestCtx) error {
+			ctx := &Context{rc}
+			return fn(ctx)
+		})
+	}
 	r.Parent.DELETE(path, func(rc *atreugo.RequestCtx) error {
 		ctx := &Context{rc}
 		return handler(ctx)
-	})
-
-	for _, fn := range middlewares {
-		r.Parent.UseBefore(func(rc *atreugo.RequestCtx) error {
-			if string(rc.Path()) == path {
-				ctx := &Context{rc}
-				return fn(ctx)
-			} else {
-				return rc.Next()
-			}
-		})
-	}
+	}).UseBefore(fns...)
 }
 
 // Use appends middlewares to router
